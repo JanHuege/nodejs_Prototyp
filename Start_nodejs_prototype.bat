3c913988b13910e599eeedeb1696f63ce41f5f2d
KhaosTheory1012 <admin@khaostheory.de>
2015-04-01 20:30:21 +0200

    Artikel und Kunde zu MDB hinzugefügt_test

--- Start_nodejs_prototype.bat
+++ Start_nodejs_prototype.bat
@@ -1,5 +1,5 @@
-start C:\Zimmermann\MongoDB\Server\3.0\bin\mongod.exe --dbpath C:\Users\Jan\WebstormProjects\nodejs_Prototyp\data
-sleep 5
+start C:\Zimmermann\MongoDB\bin\mongod.exe --dbpath C:\Users\Jan\WebstormProjects\nodejs_Prototyp\data
+sleep 1
 node C:\Users\Jan\WebstormProjects\nodejs_Prototyp\server.js
 PAUSE
 taskkill /IM mongod.exe
