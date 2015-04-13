6f96e5d0d1402439b614da51c4e5664a8fa9cf87
Jan Huege <admin@khaostheory.de>
2015-04-13 16:07:27 +0200

    Validation kunde model; vorname und alter Testweise

--- models/kunde.js
+++ models/kunde.js
@@ -1,11 +1,12 @@
 // Imports
 var mongoose = require('mongoose');
+var validators = require('mongoose-validators');
 
 // Aufbau von JSON Daten für Kunden
 var KundeSchema   = new mongoose.Schema({
   name: String,
-  vorname: String,
-  alter: Number
+  vorname: {type: String, validate: [validators.isAlpha()]},
+  alter: {type: Number, min: 0}
 });
 
 // Export the Mongoose modelf
